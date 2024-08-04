cc72688e003ebb6f201e20a008df8b382357cc6f
Yue <773639018@qq.com>
2024-08-04 18:28:16 +0800

    Update somethings.

--- package/Addition/qca/sfe/luci-app-turboacc/luasrc/controller/turboacc.lua
+++ package/Addition/qca/sfe/luci-app-turboacc/luasrc/controller/turboacc.lua
@@ -5,7 +5,7 @@ function index()
 		return
 	end
 	local page
-	page = entry({"admin", "network", "turboacc"}, cbi("turboacc"), _("Turbo ACC Center"), 101)
+	page = entry({"admin", "network", "turboacc"}, cbi("turboacc"), _("Turbo ACC Center"), 1000)
 	page.i18n = "turboacc"
 	page.dependent = true
 	
@@ -16,24 +16,14 @@ local function fastpath_status()
 	return luci.sys.call("/etc/init.d/turboacc check_status fastpath") == 0
 end
 
-local function bbr_status()
-	return luci.sys.call("/etc/init.d/turboacc check_status bbr") == 0
-end
-
 local function fullconenat_status()
 	return luci.sys.call("/etc/init.d/turboacc check_status fullconenat") == 0
-end
-
-local function dnscaching_status()
-	return luci.sys.call("/etc/init.d/turboacc check_status dns") == 0
 end
 
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 		fastpath_state = fastpath_status(),
-		fullconenat_state = fullconenat_status(),
-		bbr_state = bbr_status(),
-		dnscaching_state = dnscaching_status()
+		fullconenat_state = fullconenat_status()
 	})
 end
